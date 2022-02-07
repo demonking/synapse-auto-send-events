be21c0d0906154fe73612a7e2f2ddecc0dacbd4f
Denis Warsow <denis.warsow@xisio.com>
2022-02-07 15:47:28 +0100

    update send event

--- synapse_auto_send_events/__init__.py
+++ synapse_auto_send_events/__init__.py
@@ -30,6 +30,26 @@ from synapse.types import (
     RoomID,
 )
 
+from synapse.api.constants import (
+    EventContentFields,
+    EventTypes,
+    HistoryVisibility,
+    JoinRules,
+    Membership,
+    RoomTypes,
+)
+
+from typing import (
+    Iterable,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+    overload,
+)
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -44,35 +64,46 @@ class AutoSendEvents:
 
         self._homeserver = api._hs 
         self._room_member_handler = self._homeserver.get_room_member_handler()
+        self._room_summary_handler = self._homeserver.get_room_summary_handler()
         self._server_name  = self._homeserver.config.server.server_name
         self._store = self._homeserver.get_datastore()
+        self._event_creation_handler = self._homeserver.get_event_creation_handler()
 
         self._api.register_third_party_rules_callbacks(
-            check_event_allowed=self.check_event_allowed,
+            on_new_event=self.send_event_to_rooms,
+        )
+
+    async def is_room_a_space(self,event: EventBase):
+        if "room_id" not in event :
+            return False;
+        room_id = event.room_id
+        room_entry = await self._store.get_room_with_stats(room_id) 
+        logger.info(room_entry.keys())
+        logger.info(room_entry.values())
+        if room_entry == None:
+            return None
+
+        current_state_ids = await self._store.get_current_state_ids(room_id)
+        create_event = await self._store.get_event(
+            current_state_ids[(EventTypes.Create, "")]
         )
 
 
-    def get_event_information(self, event : EventBase): 
-        values = dict()
-        values['is_space'] = False
-        values['room_id'] = event.room_id
-
-        if "invite_room_state" not in event.unsigned:
-            return values['is_space'],values['room_id']
-
-        for entry in event.unsigned['invite_room_state']:
-            logger.info(entry)
-            if 'type' not in entry :
-                continue
-            if entry['type'] == 'm.room.create':
-                    values['is_space'] = ('type' in entry['content'] and entry['content']['type'] == 'm.space')
-        return values['is_space'],values['room_id']
+        if create_event.content.get(EventContentFields.ROOM_TYPE) == RoomTypes.SPACE :
+            return True
+        return False
 
 
-    async def check_event_allowed(self, event: EventBase, state: StateMap[EventBase]):
-        if event.is_state() and event.type == EventTypes.PowerLevels:
+    async def send_event_to_rooms(self, event: EventBase, *args: Any) -> None :
+        event_dict = event.get_dict()
+        logger.info("AUTOSENDEVENT")
+        logger.info(event_dict)
+        is_space = await self.is_room_a_space(event)
+        if(is_space == None) :
+            return is_space
 
-            is_space, room_id = self.get_event_information(event); 
+        if event.is_state() :
+
             if is_space :
                 room_id = event.room_id
                 logger.info("Event.type = %s,event.state_key=%s,event.room_id=%s",event.type,event.state_key,event.room_id)
@@ -82,8 +113,6 @@ class AutoSendEvents:
                 admin_requester = create_requester(
                     admin, authenticated_entity=requester.authenticated_entity
                 )
-                event_dict = event.get_dict()
-                logger.info(event_dict)
                 try:
                     # https://github.com/matrix-org/synapse/blob/develop/synapse/handlers/room_summary.py#L257
                     room_summary_handler =self._homeserver.get_room_summary_handler()
@@ -112,18 +141,58 @@ class AutoSendEvents:
                         content = event.content
                         # wir müssen die Zeit hier aktualisieren, sonst wird es als selber event genommen
                         # wir nehmen hier die Millisekunden da time.time() uns den Wert als Floating Point zurückgibt
-                        content['now'] = int(time.time()*1000)
-                        await self._api.create_and_send_event_into_room(
-                            {
+                        event_dict = {
                                 "room_id": event.room_id,
                                 "sender": event.sender,
                                 "type": event.type,
                                 "content": content,
                                 "state_key": "",
-                            }
-                        )
+                                "now" : int(time.time()*1000),
+                                "precision" : time.time_ns() % 1000000
+                        }
+                        #await self._api.create_and_send_event_into_room(event_dict)
+                        await self._event_creation_handler.create_and_send_nonmember_event(requester, event_dict, None)
                 except Exception as e:
                     logger.info(traceback.format_exc())
                     return None;
 
-        return True, None
+        return None 
+
+    async def resolve_room_id(
+        self, room_identifier: str, remote_room_hosts: Optional[List[str]] = None
+    ) -> Tuple[str, Optional[List[str]]]:
+        """
+        from synapse/rest/servlet.py
+        Resolve a room identifier to a room ID, if necessary.
+
+        This also performanes checks to ensure the room ID is of the proper form.
+
+        Args:
+            room_identifier: The room ID or alias.
+            remote_room_hosts: The potential remote room hosts to use.
+
+        Returns:
+            The resolved room ID.
+
+        Raises:
+            SynapseError if the room ID is of the wrong form.
+        """
+        if RoomID.is_valid(room_identifier):
+            resolved_room_id = room_identifier
+        elif RoomAlias.is_valid(room_identifier):
+            room_alias = RoomAlias.from_string(room_identifier)
+            (
+                room_id,
+                remote_room_hosts,
+            ) = await self._room_member_handler.lookup_room_alias(room_alias)
+            resolved_room_id = room_id.to_string()
+        else:
+            raise Exception(
+                400, "%s was not legal room ID or room alias" % (room_identifier,)
+            )
+        if not resolved_room_id:
+            raise Exception(
+                400, "Unknown room ID or room alias %s" % room_identifier
+            )
+        return resolved_room_id, remote_room_hosts
+
